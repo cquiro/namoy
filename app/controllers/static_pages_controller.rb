d910d22631d983f2de1213c6ab6844cf5659929d
Carlos Quiroga <cquiro@gmail.com>
2016-06-03 15:48:35 -0400

    add static pages controller

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,16 @@
+class StaticPagesController < ApplicationController
+  def about
+  end
+
+  def products
+  end
+
+  def locations
+  end
+
+  def store
+  end
+
+  def contact
+  end
+end
---
+++ spec/controllers/static_pages_controller_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe StaticPagesController, type: :controller do
+
+  describe "GET #about" do
+    it "returns http success" do
+      get :about
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #products" do
+    it "returns http success" do
+      get :products
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #locations" do
+    it "returns http success" do
+      get :locations
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #store" do
+    it "returns http success" do
+      get :store
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #contact" do
+    it "returns http success" do
+      get :contact
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
