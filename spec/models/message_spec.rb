323e4fcfed189ae361265980cca23321375dd553
Carlos Quiroga <cquiro@gmail.com>
2016-07-01 11:54:52 -0400

    Add rspec validation tests to the message model

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with a name, email, subject and message" do
+    message = Message.new(
+      name: 'Pepe Grillo', 
+      email: 'pepe@grillo.com',
+      subject: 'Test Message',
+      message: 'This is a test message for rspec. Hope it is fine.')
+
+    expect(message).to be_valid
+  end
+
+  it "is invalid without a name" do
+    message = Message.new(name: nil) 
+    message.valid?
+    expect(message.errors[:name]).to include("can't be blank")
+  end
+
+  it "is invalid without an email" do
+    message = Message.new(email: nil) 
+    message.valid?
+    expect(message.errors[:email]).to include("can't be blank")
+  end
+
+  it "is invalid without a subject" do
+    message = Message.new(subject: nil) 
+    message.valid?
+    expect(message.errors[:subject]).to include("can't be blank")
+  end
+
+  it "is invalid without a message" do
+    message = Message.new(message: nil) 
+    message.valid?
+    expect(message.errors[:message]).to include("can't be blank")
+  end
 end
