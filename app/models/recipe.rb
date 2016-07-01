7601e6ab54b93caef807a1d671a1199976cd1efe
Carlos Quiroga <cquiro@gmail.com>
2016-07-01 15:46:23 -0400

    Add validations and validations tests to recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,3 @@
 class Recipe < ActiveRecord::Base
+  validates  :name, :time_ready, :quantity, :flavor, :ingredients, :instructions, :image, presence: true
 end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -14,4 +14,46 @@ RSpec.describe Recipe, type: :model do
     expect(recipe).to be_valid
   end
 
+  it "is invalid without a name" do
+    recipe = Recipe.new(name: nil)
+    recipe.valid?
+    expect(recipe.errors[:name]).to include("can't be blank")
+  end
+
+  it "is invalid without a time_ready" do
+    recipe = Recipe.new(time_ready: nil)
+    recipe.valid?
+    expect(recipe.errors[:time_ready]).to include("can't be blank")
+  end
+
+  it "is invalid without a quantity" do
+    recipe = Recipe.new(quantity: nil)
+    recipe.valid?
+    expect(recipe.errors[:quantity]).to include("can't be blank")
+  end
+
+  it "is invalid without a flavor" do
+    recipe = Recipe.new(flavor: nil)
+    recipe.valid?
+    expect(recipe.errors[:flavor]).to include("can't be blank")
+  end
+
+  it "is invalid without ingredients" do
+    recipe = Recipe.new(ingredients: nil)
+    recipe.valid?
+    expect(recipe.errors[:ingredients]).to include("can't be blank")
+  end
+
+  it "is invalid without instructions" do
+    recipe = Recipe.new(instructions: nil)
+    recipe.valid?
+    expect(recipe.errors[:instructions]).to include("can't be blank")
+  end
+
+  it "is invalid without an image" do
+    recipe = Recipe.new(image: nil)
+    recipe.valid?
+    expect(recipe.errors[:image]).to include("can't be blank")
+  end
+
 end
