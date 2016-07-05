53950fa903e7673fc9f679cd24dd405ad1f215f2
Carlos Quiroga <cquiro@gmail.com>
2016-07-05 11:28:10 -0400

    Create index page for recipes

--- spec/factories/recipes.rb
+++ spec/factories/recipes.rb
@@ -1,11 +1,24 @@
 FactoryGirl.define do
   factory :recipe do
-    sequence(:name) { |n| "Natural#{n}" }
     time_ready "11 minutes"
     quantity "6 things"
-    flavor "natural"
     ingredients "Panela Namoy and something else"
     instructions "Mix, cool and serve."
     image "Image"
+
+    factory :natural_recipe do
+      sequence(:name) { |n| "Natural#{n}" }
+      flavor "natural"
+    end
+
+    factory :orange_recipe do
+      sequence(:name) { |n| "Naranja#{n}" }
+      flavor "naranja"
+    end
+
+    factory :lemon_recipe do
+      sequence(:name) { |n| "Limon#{n}" }
+      flavor "limon"
+    end
   end
 end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -2,61 +2,53 @@ require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
   it "is valid with name, time_ready, quantity, flavor, ingredients, instructions and image" do
-    recipe = Recipe.new(
-      name: "Cafe Helado",
-      time_ready: "10 minutos", 
-      quantity: "1 vaso", 
-      flavor: "natural", 
-      ingredients: "Cafe, helado y panela Namoy", 
-      instructions: "Ponga todos los ingredientes en un vase y revuelva", 
-      image: "Foto")
-
+    recipe = build(:lemon_recipe)
     expect(recipe).to be_valid
   end
 
   it "is invalid without a name" do
-    recipe = build(:recipe, name: nil)
+    recipe = build(:natural_recipe, name: nil)
     recipe.valid?
     expect(recipe.errors[:name]).to include("can't be blank")
   end
 
   it "is invalid without a time_ready" do
-    recipe = build(:recipe, time_ready: nil)
+    recipe = build(:natural_recipe, time_ready: nil)
     recipe.valid?
     expect(recipe.errors[:time_ready]).to include("can't be blank")
   end
 
   it "is invalid without a quantity" do
-    recipe = build(:recipe, quantity: nil)
+    recipe = build(:natural_recipe, quantity: nil)
     recipe.valid?
     expect(recipe.errors[:quantity]).to include("can't be blank")
   end
 
   it "is invalid without a flavor" do
-    recipe = build(:recipe, flavor: nil)
+    recipe = build(:natural_recipe, flavor: nil)
     recipe.valid?
     expect(recipe.errors[:flavor]).to include("can't be blank")
   end
 
   it "is invalid without ingredients" do
-    recipe = build(:recipe, ingredients: nil)
+    recipe = build(:natural_recipe, ingredients: nil)
     recipe.valid?
     expect(recipe.errors[:ingredients]).to include("can't be blank")
   end
 
   it "is invalid without instructions" do
-    recipe = build(:recipe, instructions: nil)
+    recipe = build(:natural_recipe, instructions: nil)
     recipe.valid?
     expect(recipe.errors[:instructions]).to include("can't be blank")
   end
 
   it "is invalid without an image" do
-    recipe = build(:recipe, image: nil)
+    recipe = build(:natural_recipe, image: nil)
     recipe.valid?
     expect(recipe.errors[:image]).to include("can't be blank")
   end
 
   it "has a valid factory" do
-    expect(build(:recipe)).to be_valid
+    expect(build(:natural_recipe)).to be_valid
   end  
 end
