2e2943824f2c240587f6b127394b60671ebcfc7f
Carlos Quiroga <cquiro@gmail.com>
2016-07-05 13:14:53 -0400

    Add show page and model method string_to_array

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,7 @@
 class Recipe < ActiveRecord::Base
   validates  :name, :time_ready, :quantity, :flavor, :ingredients, :instructions, :image, presence: true
+
+  def string_to_array(paragraph) #intended for breaking the string into lines
+    paragraph.split("\n")
+  end
 end
