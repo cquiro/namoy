8e30b7d5bd5726203b885afc7a60ed2e07e30658
Carlos Quiroga <cquiro@gmail.com>
2016-07-09 17:31:31 -0400

    Create partners table

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160701190059) do
+ActiveRecord::Schema.define(version: 20160709212820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "messages", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "partners", force: :cascade do |t|
+    t.string   "country"
+    t.string   "city"
+    t.string   "logo"
+    t.string   "instagram"
+    t.string   "facebook"
+    t.string   "website"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
