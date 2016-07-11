a144c005c1a58beb3db07f777cf2e749f56e7331
Carlos Quiroga <cquiro@gmail.com>
2016-07-11 15:20:18 -0400

    Add views, form, new action and new tests  for partner

--- app/controllers/partners_controller.rb
+++ app/controllers/partners_controller.rb
@@ -1,4 +1,23 @@
 class PartnersController < ApplicationController
+  before_action :set_partner, only: [:show]
+
   def index
   end
+
+  def show
+  end
+
+  def new
+    @partner = Partner.new
+  end
+
+  private
+
+    def set_partner
+      @partner = Partner.find(params[:id])
+    end
+
+    def partner_params
+      params.require(:partner).permit(:name, :country, :city, :logo, :instagram, :facebook, :website)
+    end
 end
