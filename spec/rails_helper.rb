98c73b4e99151b942c467506e3f25b60d3414af8
Carlos Quiroga <cquiro@gmail.com>
2016-07-15 17:38:50 -0400

    Separate authentication modules for controllers and for features

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,7 +30,8 @@ ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
-  config.include AuthenticationHelpers, type: :controller
+  config.include AuthenticationHelpers::Controller, type: :controller
+  config.include AuthenticationHelpers::Feature, type: :feature
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
--- spec/support/authentication_helpers.rb
+++ spec/support/authentication_helpers.rb
@@ -1,6 +1,11 @@
 module AuthenticationHelpers
-  def sign_in(user)
-    expect(controller).to receive(:current_user).and_return(user)
-    # expect(controller).to receive(:user_id).and_return(user.id)
+  module Controller
+    def sign_in(user)
+      expect(controller).to receive(:current_user).and_return(user)
+      # expect(controller).to receive(:user_id).and_return(user.id)
+    end
+  end
+
+  module Feature
   end
 end
