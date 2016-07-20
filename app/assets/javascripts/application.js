77e6eed601847b6815fbdb1890914b3cba157cff
Carlos Quiroga <cquiro@gmail.com>
2016-07-20 19:14:53 -0400

    Comment out autoslider. Replace product images. Make contact box in partners#index smaller

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -60,7 +60,7 @@ $(document).on('page:change', function() {
 
 
 // Slider
-$(document).ready(function(){
+$(document).on('page:change', function(){
   var currentIndex = 0,
     items = $('.panela-img div'),
     itemAmt = items.length;
@@ -71,16 +71,16 @@ $(document).ready(function(){
     item.css('display','inline-block');
   }
 
-  var autoSlide = setInterval(function() {
-    currentIndex += 1;
-    if (currentIndex > itemAmt - 1) {
-      currentIndex = 0;
-    }
-    cycleItems();
-  }, 3000);
+  // var autoSlide = setInterval(function() {
+  //   currentIndex += 1;
+  //   if (currentIndex > itemAmt - 1) {
+  //     currentIndex = 0;
+  //   }
+  //   cycleItems();
+  // }, 3000);
 
   $('#next').click(function() {
-    clearInterval(autoSlide);
+    // clearInterval(autoSlide);
     currentIndex += 1;
     if (currentIndex > itemAmt - 1) {
       currentIndex = 0;
@@ -89,7 +89,7 @@ $(document).ready(function(){
   });
 
   $('#prev').click(function() {
-    clearInterval(autoSlide);
+    // clearInterval(autoSlide);
     currentIndex -= 1;
     if (currentIndex < 0) {
       currentIndex = itemAmt - 1;
