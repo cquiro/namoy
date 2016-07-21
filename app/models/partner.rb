1b4aa128b3d81302531d22bade0071f24842025c
Carlos Quiroga <cquiro@gmail.com>
2016-07-21 10:17:01 -0400

    Write image_name method for Partner model

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -1,4 +1,8 @@
 class Partner < ActiveRecord::Base
   mount_uploader :logo, ImageUploader
   validates  :name, :country, :city, :logo, presence: true
+
+  def image_name
+    logo.to_s.split('/')[-1]
+  end
 end
