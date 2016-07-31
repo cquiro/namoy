1cb5ce3f15a9b2303cf76abd87b41bcfaf9f4c5c
Carlos Quiroga <cquiro@gmail.com>
2016-07-30 23:24:03 -0400

    Validate length for attributes on recipe and partner models

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -1,6 +1,9 @@
 class Partner < ActiveRecord::Base
   mount_uploader :logo, ImageUploader
   validates  :name, :country, :city, :logo, presence: true
+  validates :name, length: { maximum: 17 }
+  validates :country, length: { maximum: 11 }
+  validates :city, length: { maximum: 8 }
 
   def image_name
     logo.to_s.split('/')[-1]
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,9 @@
 class Recipe < ActiveRecord::Base
   mount_uploader :image, ImageUploader
   validates  :name, :time_ready, :quantity, :flavor, :ingredients, :instructions, :image, presence: true
+  validates :name, length: { maximum: 41 }
+  validates :time_ready, length: { maximum: 15 }
+  validates :quantity, length: { maximum: 12 }
 
   def string_to_array(paragraph) #intended for breaking the string into lines
     paragraph.split("\n")
